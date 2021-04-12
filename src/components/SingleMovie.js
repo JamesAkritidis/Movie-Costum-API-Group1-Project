b2d8a927cc832a3381514f97390c751c74de3b98
Saqer <ahmadsaqer95@outloo.com>
2021-04-12 18:21:55 +0200

    add css

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useParams} from 'react-router-dom';
 import Post from './Post';
+import cinema from '../movie_pics_small_size/cinema.jpg';
 
 function SingleMovie({movies}) {
 
@@ -10,11 +11,18 @@ function SingleMovie({movies}) {
     console.log(movie)
 
     return (
+        <div className="img2">
         <div className="post-container">
+            
             <div >
+            {/* <div className="img">
+                <img src={cinema} width="60%"></img>
+            </div> */}
             <Post item={movie} />
         </div>
 
+
+        </div>
         </div>
     )
 }
